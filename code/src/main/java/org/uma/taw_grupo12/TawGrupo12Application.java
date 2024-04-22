ec42f4cc783e69e3a666b57d5a01a983dd721d46
MVictoriaHuesca <124710728+MVictoriaHuesca@users.noreply.github.com>
2024-04-22 13:15:48 +0200

    proyecto intelij

---
+++ code/src/main/java/org/uma/taw_grupo12/TawGrupo12Application.java
@@ -0,0 +1,13 @@
+package org.uma.taw_grupo12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TawGrupo12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TawGrupo12Application.class, args);
+    }
+
+}
---
+++ code/src/test/java/org/uma/taw_grupo12/TawGrupo12ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uma.taw_grupo12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TawGrupo12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

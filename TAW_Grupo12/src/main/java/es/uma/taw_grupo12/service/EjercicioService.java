009dcea615033a1bf855a7e34dfc9b60121342a6
MVictoriaHuesca <victoriahuesca@uma.es>
2024-06-20 18:23:15 +0200

    Update EjercicioService.java

--- TAW_Grupo12/src/main/java/es/uma/taw_grupo12/service/EjercicioService.java
+++ TAW_Grupo12/src/main/java/es/uma/taw_grupo12/service/EjercicioService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class EjercicioService {
@@ -28,4 +29,5 @@ public class EjercicioService {
         return ejerciciosDTO;
     }
     //@Pablo
+
 }

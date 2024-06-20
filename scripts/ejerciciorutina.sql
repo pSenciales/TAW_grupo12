c1cd2dd95a205043ebde11f27e0e38e8b1df91ad
WillyJoJo <guillermopp@uma.es>
2024-06-20 17:46:45 +0200

    scripts y cambio de tablas cliente

---
+++ scripts/ejerciciorutina.sql
@@ -0,0 +1,10 @@
+INSERT INTO taw12.ejerciciorutina (idejerciciorutina, idrutina, idejercicio, peso, repeticiones, series, orden, diassemana) VALUES (2, 1, 18, '', 20, 2, 0, 'Lunes');
+INSERT INTO taw12.ejerciciorutina (idejerciciorutina, idrutina, idejercicio, peso, repeticiones, series, orden, diassemana) VALUES (6, 5, 7, '100 m', 1, 4, 0, 'Lunes');
+INSERT INTO taw12.ejerciciorutina (idejerciciorutina, idrutina, idejercicio, peso, repeticiones, series, orden, diassemana) VALUES (7, 5, 4, '10 largos', 2, 3, 1, 'Lunes');
+INSERT INTO taw12.ejerciciorutina (idejerciciorutina, idrutina, idejercicio, peso, repeticiones, series, orden, diassemana) VALUES (8, 1, 8, '', 2, 2, 0, 'Martes');
+INSERT INTO taw12.ejerciciorutina (idejerciciorutina, idrutina, idejercicio, peso, repeticiones, series, orden, diassemana) VALUES (9, 1, 32, '', 1, 2, 1, 'Miércoles');
+INSERT INTO taw12.ejerciciorutina (idejerciciorutina, idrutina, idejercicio, peso, repeticiones, series, orden, diassemana) VALUES (11, 1, 4, '40 largos', 1, 2, 1, 'Lunes');
+INSERT INTO taw12.ejerciciorutina (idejerciciorutina, idrutina, idejercicio, peso, repeticiones, series, orden, diassemana) VALUES (13, 6, 5, '50 km', 1, 1, 0, 'Lunes');
+INSERT INTO taw12.ejerciciorutina (idejerciciorutina, idrutina, idejercicio, peso, repeticiones, series, orden, diassemana) VALUES (14, 1, 7, '30 m', 5, 2, 0, 'Miércoles');
+INSERT INTO taw12.ejerciciorutina (idejerciciorutina, idrutina, idejercicio, peso, repeticiones, series, orden, diassemana) VALUES (15, 1, 17, '', 10, 4, 0, 'Jueves');
+INSERT INTO taw12.ejerciciorutina (idejerciciorutina, idrutina, idejercicio, peso, repeticiones, series, orden, diassemana) VALUES (16, 1, 29, '30 min', 1, 1, 0, 'Viernes');

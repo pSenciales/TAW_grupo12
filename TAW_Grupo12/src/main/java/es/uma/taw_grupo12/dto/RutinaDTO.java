570d6d44110ef1393220a5db2e2b1c8f456104df
MVictoriaHuesca <victoriahuesca@uma.es>
2024-06-20 18:16:15 +0200

    Update RutinaDTO.java

--- TAW_Grupo12/src/main/java/es/uma/taw_grupo12/dto/RutinaDTO.java
+++ TAW_Grupo12/src/main/java/es/uma/taw_grupo12/dto/RutinaDTO.java
@@ -1,16 +1,25 @@
 package es.uma.taw_grupo12.dto;
 
-import es.uma.taw_grupo12.entity.Cliente;
-import es.uma.taw_grupo12.entity.EjercicioRutina;
-import jakarta.persistence.*;
+import es.uma.taw_grupo12.entity.Rutina;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class RutinaDTO {
+    private Integer idtrabajador;
     private Integer idrutina;
     private String nombre;
     private List<Integer> ejercicioRutinaList;
     private Integer idcliente;
+
+    public RutinaDTO(String nombre, Integer idtrabajador, Integer idcliente) {
+        this.nombre = nombre;
+        this.idtrabajador = idtrabajador;
+        this.idcliente = idcliente;
+    }
+
+    public RutinaDTO() {
+
+    }
 }

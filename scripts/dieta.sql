adea174ec82671db431081cb8bcdaa92504a5949
lx2708 <64492657+lx2708@users.noreply.github.com>
2024-06-23 15:22:02 +0200

    script para dietista

---
+++ scripts/dieta.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM taw12.dieta
+LIMIT 0, 1000
+
+-- Date: 2024-06-22 11:21
+*/
+INSERT INTO `` (`iddieta`,`nombre`,`idcliente`) VALUES (59,'Dieta para Juan',1);
+INSERT INTO `` (`iddieta`,`nombre`,`idcliente`) VALUES (60,'Dieta 2 para Juan',1);
